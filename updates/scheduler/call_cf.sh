c8864008c2085de172d7914974ca171e95b99022
David Stroud <david@davidstroud.me>
2018-12-20 17:05:13 -0500

    first commit

---
+++ updates/scheduler/call_cf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+REGION='us-central1'
+PROJECT=$(gcloud config get-value project)
+BUCKET=cloud-training-demos-ml
+URL=ingest_flights_udwaxx86mVygAmOazUcijW8zBXWNxEVM
+
+echo {\"year\":\"2015\"\,\"month\":\"03\"\,\"bucket\":\"${BUCKET}\"} > /tmp/message
+cat /tmp/message
+
+curl -X POST "https://${REGION}-${PROJECT}.cloudfunctions.net/$URL" -H "Content-Type:application/json" --data-binary @/tmp/message
+
